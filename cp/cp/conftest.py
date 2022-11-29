eaaf357e6a94b4a3be3d21db109d18aaa00c1ae3
prathamiitp <prathamchoudhary4@gmail.com>
2022-11-29 20:09:09 +0530

    setup

---
+++ cp/cp/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from cp.users.models import User
+from cp.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
